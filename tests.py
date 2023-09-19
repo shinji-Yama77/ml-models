44f19d04274d66f77a719aa3f0264dd6853aac93
Shinji Yamashita <shinjiyamashita@Shinjis-MacBook-Pro.local>
2023-09-19 18:11:59 +0800

    w

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 import numpy as np
-from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import pandas as pd
 df = pd.read_csv('healthcare.csv')
+
 
 from decisiontree import ID3
 
